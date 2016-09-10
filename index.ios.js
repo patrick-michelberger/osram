d2150282364cea423f2f726111a88728ab496bc1
Daniel Elsner <daniel.elsner@tum.de>
2016-09-10 11:35:31 +0200

    initial style with linear gradient

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -11,21 +5,24 @@ import {
   Text,
   View
 } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 class osram extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <LinearGradient colors={['#FFB88C' , '#DE6262']} style={styles.linearGradient}>
+          <Text style={styles.welcome}>
+            Welcome to React Native!
+          </Text>
+          <Text style={styles.instructions}>
+            To get started, edit index.ios.js
+          </Text>
+          <Text style={styles.instructions}>
+            Press Cmd+R to reload,{'\n'}
+            Cmd+D or shake for dev menu
+          </Text>
+        </LinearGradient>
       </View>
     );
   }
@@ -33,10 +30,8 @@ class osram extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flex: 1 ,
+    backgroundColor: 'transparent'
   },
   welcome: {
     fontSize: 20,
@@ -47,6 +42,12 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  linearGradient: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center',
   },
 });
 
